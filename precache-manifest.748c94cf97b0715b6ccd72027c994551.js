62fc69ac3da1cda3f1e2190d87ebfb8695733696
veeru153 <32760644+veeru153@users.noreply.github.com>
2020-07-01 23:59:59 +0530

    Removed source code. Moved build files to root

---
+++ precache-manifest.748c94cf97b0715b6ccd72027c994551.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "feeb5492e68ed243b98852b048c85265",
+    "url": "/true-desi-test/index.html"
+  },
+  {
+    "revision": "449979e11010e10a15d0",
+    "url": "/true-desi-test/static/css/main.19d5f3ae.chunk.css"
+  },
+  {
+    "revision": "ab907a6b0b269485dd1b",
+    "url": "/true-desi-test/static/js/2.74f9c19f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/true-desi-test/static/js/2.74f9c19f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "449979e11010e10a15d0",
+    "url": "/true-desi-test/static/js/main.53ac5c14.chunk.js"
+  },
+  {
+    "revision": "d1069e0b59390ff3a6a3",
+    "url": "/true-desi-test/static/js/runtime-main.b3c6c470.js"
+  },
+  {
+    "revision": "00459ea053f184b48ee67e5d8696c55b",
+    "url": "/true-desi-test/static/media/header.00459ea0.png"
+  },
+  {
+    "revision": "a790fb529220e79cf5814b8df1b33f85",
+    "url": "/true-desi-test/static/media/thali.a790fb52.png"
+  }
+]);
